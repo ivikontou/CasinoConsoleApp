4392f5389b7659f9fb4b64fd7591e4c59b979fba
Nikoletta <p3230055@aueb.gr>
2026-05-07 19:23:03 +0300

    activity_main.xml

--- app/src/main/java/com/example/casinoconsoleapp/MainActivity.kt
+++ app/src/main/java/com/example/casinoconsoleapp/MainActivity.kt
@@ -1,47 +1,32 @@
 package com.example.casinoconsoleapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.casinoconsoleapp.ui.theme.CasinoConsoleAppTheme
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CasinoConsoleAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        // Φορτώνει το XML που μόλις έφτιαξες
+        setContentView(R.layout.activity_main)
+
+        val etPlayerName = findViewById<EditText>(R.id.etPlayerName)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val name = etPlayerName.text.toString().trim()
+            if (name.isNotEmpty()) {
+                // Σε πηγαίνει στο DashboardActivity.java
+                val intent = Intent(this, DashboardActivity::class.java)
+                intent.putExtra("PLAYER_NAME", name)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Please enter your name!", Toast.LENGTH_SHORT).show()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CasinoConsoleAppTheme {
-        Greeting("Android")
     }
 }
